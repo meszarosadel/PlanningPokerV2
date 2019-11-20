b34bb365752aebfd9ad986c5656f8e301a564577
Szilvia97 <szilvia_krisz_tina@yahoo.com>
2019-11-20 17:43:23 +0200

    Login-Regi

--- app/src/main/java/com/example/planningpokerfb/FragmentLogin.java
+++ app/src/main/java/com/example/planningpokerfb/FragmentLogin.java
@@ -25,9 +25,10 @@ public class FragmentLogin extends Fragment {
 
     EditText et_email;
     EditText et_pwd;
-    Button button;
+    Button btn_login;
     TextView tv_reg;
     FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,13 +38,13 @@ public class FragmentLogin extends Fragment {
         // Inflate the layout for this fragment
         et_email = v.findViewById(R.id.et_email);
         et_pwd = v.findViewById(R.id.et_pwd);
-        button = v.findViewById(R.id.button);
+        btn_login = v.findViewById(R.id.btn_login);
         tv_reg = v.findViewById(R.id.tv_reg);
 
         final Context context = getActivity();
 
 
-        button.setOnClickListener(new View.OnClickListener() {
+        btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = et_email.getText().toString();
@@ -91,6 +92,11 @@ public class FragmentLogin extends Fragment {
 
         return v;
 
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
 
 }
--- app/src/main/java/com/example/planningpokerfb/FragmentReg.java
+++ app/src/main/java/com/example/planningpokerfb/FragmentReg.java
@@ -1,19 +1,94 @@
 package com.example.planningpokerfb;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class FragmentReg extends Fragment {
 
+
+    EditText et_email, et_pwd;
+    Button btn_reg;
+    TextView tv_sign;
+    FirebaseAuth mFirebaseAuth;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_fragment_login, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_reg, container, false);
+        et_email = v.findViewById(R.id.et_email);
+        et_pwd = v.findViewById(R.id.et_pwd);
+        btn_reg = v.findViewById(R.id.btn_login);
+        tv_sign = v.findViewById(R.id.tv_reg);
+
+        final Context context = getActivity();
+
+        btn_reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = et_email.getText().toString();
+                String pwd = et_pwd.getText().toString();
+                if(email.isEmpty()){
+                    et_email.setError("Please enter email id");
+                    et_email.requestFocus();
+                }
+                else  if(pwd.isEmpty()){
+                    et_pwd.setError("Please enter your password");
+                    et_pwd.requestFocus();
+                }
+                else  if(email.isEmpty() && pwd.isEmpty()){
+                    Toast.makeText(getActivity(),"Fields Are Empty!",Toast.LENGTH_SHORT).show();
+                }
+                else  if(!(email.isEmpty() && pwd.isEmpty())){
+                    mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(getActivity(),"SignUp Unsuccessful, Please Try Again",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+                else{
+                    Fragment fragment = new FragmentUser();
+                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.frame_id, fragment);
+                    fragmentTransaction.addToBackStack(null);
+                    fragmentTransaction.commit();
+                }
+            }
+        });
+
+        tv_sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new FragmentLogin();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame_id, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return v;
     }
 }
